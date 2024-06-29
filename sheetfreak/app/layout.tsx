6eb36c13215523f75eab1d3921ee9aeaebf10dd7
Kyle <kyle.tz.cui@gmail.com>
2024-06-29 15:45:42 -0700

    rename page title

--- sheetfreak/app/layout.tsx
+++ sheetfreak/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "freakinthesheets",
+  title: "sheetfreak",
   description: "CalHacks x Skydeck AI Hackathon",
 };
 
