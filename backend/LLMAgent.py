af9b9dda36f352b83aebf717e6b2bbcc21c08399
Kyle <kyle.tz.cui@gmail.com>
2024-06-30 15:40:57 -0700

    grind

--- backend/LLMAgent.py
+++ backend/LLMAgent.py
@@ -1,21 +1,30 @@
 import os
 import json
+from TableAgent import *
 from gpt_function_tools import *
 from claude_function_tools import *
 
 from openai import OpenAI
 import boto3
 
-gpt_model_to_model_IDs = {
-    # Must start with 'gpt'
+# gpt_model_to_model_IDs = {
+#     # Must start with 'gpt'
+#     "gpt-4o": "gpt-4o",
+#     "gpt-4-turbo": "gpt-4-turbo",
+#     "gpt-4": "gpt-4",
+#     "gpt-3.5": "gpt-3.5-turbo",
+# }
+
+# claude_model_to_model_IDs = {
+#     # Must start with 'claude'
+#     "claude-3.5": "anthropic.claude-3-5-sonnet-20240620-v1:0",
+# }
+
+model_to_model_IDs = {
     "gpt-4o": "gpt-4o",
     "gpt-4-turbo": "gpt-4-turbo",
     "gpt-4": "gpt-4",
     "gpt-3.5": "gpt-3.5-turbo",
-}
-
-claude_model_to_model_IDs = {
-    # Must start with 'claude'
     "claude-3.5": "anthropic.claude-3-5-sonnet-20240620-v1:0",
 }
 
@@ -28,48 +37,39 @@ tools = {
     "other_instruction"
 }
 
+instruction_type_to_tool_name = {
+    "WRITE": "write_table",
+    "READ": "read_table",
+    "CHART": "create_chart",
+    "QUESTION": "question",
+    "OTHER": "other_instruction",
+}
+
 # TODO: timeit measure latency of class methods
 class LLMAgent:
     """LLMAgent is the orchestrated agent responsible for making LLM calls to plan and produce instructions"""
 
     def __init__(self, default_call="gpt", default_gpt_model="gpt-4o", default_claude_model="claude-3.5"):
-        # Maps tool's function name to model to use for that tool
-        self.tools_to_models = {}
-        self.default_call = default_call
+        self.tools_to_models = {} # Maps tool's function name to model to use for that tool
+        self.max_attempts = 7
+        self.default_call = default_call # Either 'gpt' or 'claude'
         self.default_gpt_model = default_gpt_model
         self.default_claude_model = default_claude_model
-        self.openai_client = None
-        self.bedrock_client = None
-        # self.openai_client_initialized = False
+        # self.openai_client = None
+        # self.bedrock_client = None
         self.openai_client = OpenAI(organization=os.environ["OPENAI_ORG"])
-        # self.openai_client_initialized = True
-        # self.bedrock_client_initialized = False
         self.bedrock_client = boto3.client(service_name='bedrock-runtime', region_name='us-east-1',
                                 aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
                                 aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
                                 )
-        # self.bedrock_client_initialized = True
-
-    # def initialize_openai_client(self):
-    #     """Initializes LLM clients to use"""
-    #     self.openai_client = OpenAI(organization=os.environ["OPENAI_ORG"])
-    #     self.openai_client_initialized = True
-    #     print("Initialized OpenAI client")
-    
-    # def initialize_bedrock_client(self):
-    #     self.bedrock_client = boto3.client(service_name='bedrock-runtime', region_name='us-east-1',
-    #                             aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-    #                             aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
-    #                             )
-    #     self.bedrock_client_initialized = True
-    #     print("Initialized boto3 client")
     
     def set_tools_to_models(self, key, value):
         """Sets the model to use for the given tool"""
         if key not in tools:
             print("Invalid tool!")
             return
-        if value not in gpt_model_to_model_IDs and value not in claude_model_to_model_IDs:
+        # if value not in gpt_model_to_model_IDs and value not in claude_model_to_model_IDs:
+        if value not in model_to_model_IDs:
             print("Invalid model!")
             return
         self.tools_to_models[key] = value
@@ -79,26 +79,26 @@ class LLMAgent:
         """Returns model ID to use for the given tool_name"""
         if tool_name in self.tools_to_models:
             model_name = self.tools_to_models[tool_name]
-            if model_name.startswith("gpt"):
-                return gpt_model_to_model_IDs[model_name]
-            elif model_name.startswith("claude"):
-                return claude_model_to_model_IDs[model_name]
+            # if model_name.startswith("gpt"):
+            #     return gpt_model_to_model_IDs[model_name]
+            # elif model_name.startswith("claude"):
+            #     return claude_model_to_model_IDs[model_name]
+            return model_to_model_IDs[model_name]
         else:
             if self.default_call == "gpt":
                 model_name = self.default_gpt_model
-                return gpt_model_to_model_IDs[model_name]
+                # return gpt_model_to_model_IDs[model_name]
+                return model_to_model_IDs[model_name]
             elif self.default_call == "claude":
                 model_name = self.default_claude_model
-                return claude_model_to_model_IDs[model_name]
+                # return claude_model_to_model_IDs[model_name]
+                return model_to_model_IDs[model_name]
             else:
                 print("Could not find LLM model to use")
         return ""
     
     def call_gpt(self, model_ID, user_msg_content, tool_name):
         """Call GPT on OpenAI"""
-        # if not self.openai_client_initialized:
-        #     self.initialize_openai_client()
-
         tool, sys_msg = gpt_tools["gpt_" + tool_name]
         user_msg = {
             "role": "user",
@@ -117,9 +117,6 @@ class LLMAgent:
     
     def call_claude(self, model_ID, user_msg_content, tool_name):
         """Call Claude on AWS Bedrock"""
-        # if not self.bedrock_client_initialized:
-        #     self.initialize_bedrock_client()
-
         tool, sys_msg = claude_tools["claude_" + tool_name]
         messages = [{"role": "user", "content": user_msg_content}]
         body = json.dumps({
@@ -135,116 +132,132 @@ class LLMAgent:
         print(response_body['content'])
         return response_body['content']
 
-    def get_instructions(self, task_prompt, sheet_content):
-        """Attempts to get list of instructions to complete task"""
-        tool_name = "get_instructions"
-        get_instructions_user_msg = "Table:\n" + sheet_content.to_string() + f"\nEnd Table.\nTasks:\n{task_prompt}"
+    def get_instruction_args(self, tool_name, task, sheet_content, args_names, prev_response="", prev_response_error=""):
+        """Gets instructions arguments.
+        Returns success bool, error message, and args.
+        """
+        user_msg = "Table:\n" + sheet_content + f"\nEnd Table.\nInstructions:\n{task}"
+        if prev_response:
+            user_msg += f"\nYour previous response was {prev_response} which resulted in an error."
+        if prev_response_error:
+            user_msg += f"\nThe error was: {prev_response_error}"
         model_ID = self.get_model_ID(tool_name)
         if model_ID.startswith("gpt"):
-            gpt_response = self.call_gpt(model_ID, get_instructions_user_msg, tool_name)
-            get_instructions_tool_calls = gpt_response.tool_calls
-            instruction_types = []
-            instruction_commands = []
-            for i in range(len(get_instructions_tool_calls)):
-                get_instructions_args = get_instructions_tool_calls[i].function.arguments
-                print("Get instructions args:", get_instructions_args)
-                args = json.loads(get_instructions_args)
-                instruction_types += args["types"]
-                instruction_commands += args["instructions"]
-            if len(instruction_types) != len(instruction_commands):
-                print("Invalid instructions!")
-                return []
-            instructions = []
-            for i in range(len(instruction_types)):
-                instructions.append((instruction_types[i], instruction_commands[i]))
-            return instructions
+            gpt_response = self.call_gpt(model_ID, user_msg, tool_name)
+            tool_calls = gpt_response.tool_calls
+            args_collection = [None for _ in range(len(args_names))]
+            for i in range(len(tool_calls)):
+                instruction_args = tool_calls[i].function.arguments
+                print(f"{tool_name} function args:", instruction_args)
+                args = json.loads(instruction_args)
+                for j in range(len(args_names)):
+                    if not args_collection[j]:
+                        args_collection[j] = args[args_names[j]]
+                    else:
+                        if type(args_collection[j] == str):
+                            args_collection[j] += " " + args[args_names[j]]
+                        else:
+                            args_collection[j] += args[args_names[j]]
+                    if j > 0 and type(args_collection[j]) == list and type(args_collection[j-1]) == list:
+                        if len(args_collection[j]) != len(args_collection[j-1]):
+                            print("Invalid instructions")
+                            return False, "Invalid instructions length", instruction_args
+            if type(args_collection[0]) == str:
+                return True, "", args_collection
+            assert(type(args_collection[0] == list))
+            instruction_args = []
+            for i in range(len(args_collection[0])):
+                curr_instruction = [args_collection[j][i] for j in range(len(args_names))]
+                instruction_args.append(curr_instruction)
+            return True, "", instruction_args
         elif model_ID.startswith("anthropic"):
-            claude_response = self.call_claude(model_ID, get_instructions_user_msg, tool_name)
-            # TODO: Claude get_instructions
+            claude_response = self.call_claude(model_ID, user_msg, tool_name)
+            # TODO: claude instructions
         else:
-            print("Sorry, there was an error finding an LLM to use")
-            return "Sorry, there was an error finding an LLM to use"
+            print("Invalid LLM")
+            return False, "Invalid LLM", ""
+
+    def get_arg_names(self, instruction_type):
+        if instruction_type == "get_instructions":
+            return ["types", "instructions"]
+        elif instruction_type == "WRITE":
+            return ["rows", "columns", "values"]
+        elif instruction_type == "READ":
+            return ["rows", "columns"]
+        elif instruction_type == "CHART":
+            return ["arguments"]
+        elif instruction_type == "QUESTION":
+            return ["answer"]
+        elif instruction_type == "OTHER":
+            return ["body"]
     
-    def read_instruction(self, sheet_content, instruction_command):
-        """Performs a read instruction by reading from the table"""
-        tool_name = "read_table"
-        read_table_user_msg = "Table:\n" + sheet_content.to_string() + f"\nEnd Table.\nInstructions:\n{instruction_command}"
-        model_ID = self.get_model_ID(tool_name)
-        if model_ID.startswith("gpt"):
-            gpt_response = self.call_gpt(model_ID, read_table_user_msg, tool_name)
-            read_table_tool_calls = gpt_response.tool_calls
-            
-    
-    def act_streamer(self, task_prompt: str, sheet_content: str):
+    def act_streamer(self, task_prompt: str, sheet_id: str, sheet_range: str):
         """Attempts to complete given task prompt and streams outputs"""
+        table_agent = TableAgent(sheet_id)
+        sheet_content = table_agent.get_sheet_content(sheet_range)
         # 1. Get instructions
-        instructions = self.get_instructions(task_prompt, sheet_content)
+        prev_response = None
+        prev_response_error = None
+        instructions = None
+        for attempt_num in range(1, self.max_attempts+1):
+            try:
+                print(f"Attempt {attempt_num} of get_instructions")
+                success, error_msg, args = self.get_instruction_args("get_instructions", task_prompt, sheet_content, self.get_arg_names("get_instructions"), prev_response, prev_response_error)
+                if not success:
+                    assert(type(error_msg) == type(args) == str)
+                    prev_response = args
+                    prev_response_error = error_msg
+                    print("Error in get_instructions", error_msg)
+                    continue
+                else:
+                    instructions = args
+                    break
+            except:
+                print("Error in get_instructions")
+                continue
         print("Instructions:", instructions)
         print_instructions = " ".join([f"{instr[1]}" for instr in instructions])
         yield print_instructions
 
         # 2. Execute instructions
+        need_to_push_sheet_content = False
         for instruction in instructions:
             print("Executing", instruction)
-            yield instruction[1]
-
-            instruction_type = instruction[0]
-            instruction_command = instruction[1]
-            if instruction_type == "READ":
-                read_values = self.read_instruction(sheet_content, instruction_command)
-                if read_values:
-                    clean_read_values = [s for s in read_values if s]
-                    print_read = ", ".join(clean_read_values)
-                    yield print_read
-                else:
-                    yield "Sheet read failed."
-                    return
-            elif instruction_type == "WRITE":
-                new_sheet_content, success, instructions_wrote = write_instruction(sheet_content, instruction_command)
-                yield get_chunk_to_yield(f"Attempted writing...\n{instructions_wrote}")
-                if not success:
-                    print("Couldn't complete write instruction within allowed attempts")
-                    yield get_chunk_to_yield("Sheet write failed.")
-                    return
-                sheet_content = new_sheet_content
-                write_sheet(sheets, sheet_id, sheet_range, sheet_content)
-                chat_response += "Sheet write successful. "
-
-            elif instruction_type == "CHART":
-                chart_instruction_response = chart_instruction(sheet_content, instruction_command, creds, sheet_id)
-                if chart_instruction_response:
-                    yield get_chunk_to_yield(f"Attempted making a chart with given schema...")
-                    chat_response += "Chart creation successful. "
-                else:
-                    yield get_chunk_to_yield("Chart creation failed.")
-                    return
-
-            elif instruction_type == "OTHER":
-                other_instruction_response = other_instruction(sheet_content, instruction_command, creds, sheet_id)
-                if other_instruction_response:
-                    yield get_chunk_to_yield(f"Attempted completing command\n{instruction_command}")
-                    chat_response += "Instruction successful. "
-                else:
-                    yield get_chunk_to_yield("Instruction failed.")
-                    return
-
-            elif instruction_type == "QUESTION":
-                question_res = question_instruction(sheet_content, instruction_command, creds, sheet_id)
-                yield get_chunk_to_yield(question_res)
-
-            elif instruction_type == "INAPPROPRIATE":
-                chat_response += "This is irrelevant to Google Sheets. "
-                yield get_chunk_to_yield(f"Sorry I can't help with: {instruction_command}")
-            else:
-                print("Unrecognizable instruction!")
-                
-        print("Finished all instructions")
-        yield get_chunk_to_yield("Finished executing instructions. " + chat_response)
-        return
-
-    
-    def hi(self):
-        # self.set_tools_to_models("create_chart", "gpt-3.5")
-        # self.call_gpt("create a blue line graph of columns 1 to 10", "create_chart")
-        self.set_tools_to_models("create_chart", "claude-3.5")
-        self.call_claude("create a blue line graph of columns 1 to 10", "create_chart")
+            yield f"Executing...\n{instruction[1]}"
+            prev_response = None
+            prev_response_error = None
+            for attempt_num in range(1, self.max_attempts+1):
+                try:
+                    instruction_type = instruction[0]
+                    instruction_command = instruction[1]
+                    print(f"Attempt {attempt_num} of {instruction_type}: {instruction_command}")
+                    if instruction_type == "INAPPROPRIATE":
+                        yield "Sorry I can't help with that..."
+                        break
+                    if instruction_type not in instruction_type_to_tool_name:
+                        print("Unrecognized instruction type")
+                        break
+                    
+                    success, error_msg, args = self.get_instruction_args(instruction_type_to_tool_name[instruction_type], instruction_command, sheet_content, self.get_arg_names(instruction_type), prev_response, prev_response_error)
+                    if not success:
+                        assert(type(error_msg) == type(args) == str)
+                        prev_response = args
+                        prev_response_error = error_msg
+                        print("Error:", error_msg)
+                        continue
+                    success, error_msg, result = table_agent.execute_instruction(instruction_type, args)
+                    if not success:
+                        assert(type(error_msg) == type(result) == str)
+                        prev_response_error = error_msg
+                        print("Error:", error_msg)
+                        continue
+                    if instruction_type == "WRITE":
+                        need_to_push_sheet_content = True
+                    yield result
+                    break
+                except Exception as e:
+                    prev_response = None
+                    prev_response_error = None
+                    continue
+        if need_to_push_sheet_content:
+            table_agent.push_sheet_content(sheet_range)
--- backend/TableAgent.py
+++ backend/TableAgent.py
@@ -52,7 +52,7 @@ class TableAgent:
         share_link = file.get('webViewLink')
         return share_link
 
-    def get_sheets_content(self, sheet_range):
+    def get_sheet_content(self, sheet_range):
         """Gets content of sheet ID"""
         read_sheet_result = (
             self.sheets_service.spreadsheets().values()
@@ -64,3 +64,128 @@ class TableAgent:
         print("Read values:", sheet_content)
         self.sheet_content = sheet_content
         return sheet_content.to_string()
+    
+    def push_sheet_content(self, sheet_range):
+        """Writes sheet content back to online Google Sheets file"""
+        write_sheet_body = {
+            "values": self.sheet_content.values.tolist()
+        }
+        write_sheet_result = (
+            self.sheets_service.spreadsheets().values()
+            .update(
+                spreadsheetId=self.sheet_id,
+                range=sheet_range,
+                valueInputOption="USER_ENTERED",
+                body=write_sheet_body,
+            )
+            .execute()
+        )
+        print(write_sheet_result)
+    
+    def expand_table(self, newRows, newCols):
+        """Expand the table to size newRows x newCols"""
+        currRows = len(self.sheet_content)
+        currCols = len(self.sheet_content.columns)
+        if newCols < currCols and newRows < currRows:
+            return
+        
+        print(f"Expanding table to {newRows+1} x {newCols+1}")
+        if newCols >= currCols:
+            for i in range(currCols, newCols+1):
+                self.sheet_content[i] = [pd.NA for _ in range(currRows)]
+            currCols = newCols+1
+        if newRows >= currRows:
+            for i in range(currRows, newRows + 1):
+                self.sheet_content.loc[i] = [pd.NA for _ in range(currCols)]
+        return
+
+    def write_table(self, args):
+        """Write the table at the given rows and columns to the given values"""
+        rows = args[0]
+        columns = args[1]
+        values = args[2]
+        
+        maxRows = max(rows)
+        maxCols = max(columns)
+        self.expand_table(maxRows, maxCols)
+
+        n = len(rows)
+        for i in range(n):
+            print(f"Setting {rows[i]}, {columns[i]} to {values[i]}")
+            self.sheet_content.iloc[rows[i], columns[i]] = values[i]
+        print("Final sheet:", self.sheet_content)
+
+    def read_table(self, args):
+        """Gets the table values at the specified rows and columns"""
+        rows = args[0]
+        columns = args[1]
+        
+        maxRows = max(rows)
+        maxCols = max(columns)
+        self.expand_table(maxRows, maxCols)
+        
+        returned_values = []
+        n = len(rows)
+        for i in range(n):
+            returned_values.append(self.sheet_content.iloc[rows[i], columns[i]])
+        print("Read in", returned_values)
+        return returned_values
+    
+    def create_chart(self, args):
+        """Creates a chart"""
+        format_arguments = lambda x: json.loads(x) if type(x)==str else x
+        create_chart_request = {
+            "requests": [
+                {
+                    "addChart": {
+                        "chart": {
+                            "spec": {
+                                "title": args[0],
+                                
+                                "basicChart": {
+                                    "chartType": args[1],
+                                    "legendPosition": args[2],
+                                    "axis": format_arguments(args[3]),
+                                    "domains": format_arguments(args[4]),
+                                    "series": format_arguments(args[5]),
+                                }
+                            },
+                            "position": format_arguments(args[6]),
+                        }
+                    }
+                }
+            ]
+        }
+
+        print("Creating chart with", create_chart_request)
+        create_chart_response = self.sheets_service.spreadsheets().batchUpdate(spreadsheetId=self.sheet_id, body=create_chart_request).execute()
+        print("Finished operation", create_chart_response)
+    
+    def other_instruction(self, args):
+        """Performs other instruction via spreadsheets.batchUpdate()"""
+        other_instruction_response = self.sheets_service.spreadsheets().batchUpdate(
+            spreadsheetId=self.sheet_id, body=args[0]
+            ).execute()
+        print("Executed other instruction with", args)
+    
+    def execute_instruction(self, instruction_type, args):
+        print(f"Attempting {instruction_type} with {args}")
+        try:
+            if instruction_type == "WRITE":
+                self.write_table(args)
+                return True, "", "Finished writing to table"
+            elif instruction_type == "READ":
+                vals = self.read_table(args)
+                return True, "", vals
+            elif instruction_type == "CHART":
+                self.create_chart(args)
+                return True, "", "Created chart"
+            elif instruction_type == "QUESTION":
+                return True, "", args
+            elif instruction_type == "OTHER":
+                self.other_instruction(args)
+                return True, "", "Completed instruction"
+            
+        except Exception as e:
+            print("Exception", e)
+            return False, e, ""
